881abd3ebc51a66360a4565e6bdfaa52a7b205e4
neusgramunt <neus.gramunt01@estudiant.upf.edu>
2022-04-27 12:19:37 +0200

    Volume and images

--- Audiovisual Project 2/main.py
+++ Audiovisual Project 2/main.py
@@ -16,7 +16,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script. LALALALALALA
+# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Projecte audiovisual')
 
